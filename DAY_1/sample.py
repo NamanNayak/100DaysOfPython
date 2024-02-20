3753f74a85beab9c3e2e3ea6ea8a802add68292b
Naman Nayak <namannayak1919@gmail.com>
2024-02-20 23:28:28 +0530

    Day 1

---
+++ DAY_1/sample.py
@@ -0,0 +1,25 @@
+# Print anything
+print("hello")
+
+
+# String Concatination
+print("I"+" "+"am "+"a Programmar")
+
+
+#Input function
+print("Hi, "+ input("What's ur Name??\n"))
+
+
+#Print Length of the input
+x = str(input("Type ur word to know its length\n"))
+print(len(x))
+
+
+#Variables
+x = input("Enter ur first name")
+y = input("Enter ur last name")
+temp = y
+y = x
+x = temp
+print(x)
+print(y)
