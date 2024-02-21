7a04c9aa238bc41e8d272071ef94e8011afcc952
Naman Nayak <namannayak1919@gmail.com>
2024-02-21 23:11:01 +0530

    DAY_2

---
+++ DAY_2/data_types.py
@@ -0,0 +1,40 @@
+#Data types
+
+# integers
+x = 3
+print(type(x))
+
+# float
+y = 3.5
+print(type(y))
+
+# String
+z = "3848"
+print(type(z))
+
+
+#Type Casting
+x = 244
+x = str(x)
+print(x)
+print(type(x))
+
+x = float(x)
+print(x)
+print(type(x))
+
+print(200+float("29.99999"))
+
+#Adding two digit number
+x = int(input("Type two digit number"))
+y = x / 10
+z = x - int(y)*10
+print(int(y)+int(z))
+
+
+x = input("Type two digit number")
+first = int(x[0])
+second = int(x[1])
+print(first+second)
+
+
